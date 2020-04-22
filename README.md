1b0cab9359d45f716c00c4bf2edcb8085ca28ab0
luke-ols0 <luke.olson@outlook.com>
2020-04-22 00:46:26 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+## Write a C++ program that does the following :
+
+> 1. Accepts array size from the keyboard. The size must be positive integer that is > = 5 - *Done*
+
+> 2. Use the size from step 1 in order to create an integer array. Populate the created array  with random integer values between 10 and 375. - *Done* 
+
+> 3. Display the generated array. - *Done*
+
+> 4. Write a function that uses recursion in order to display the array in reverse order - *Done*
+
+> 5. Write a function that uses recursion in order to display squares of integers in
+> ascending order, starting from 1 to first number in the array. - *Done* 
+
+> 6. Write a recursive function that takes that integer n and returns the sum of the digits
+> of that integer - *Done? vague instructions*
+
+> 7. Write a function that uses recursion in order to determine whether or not every
+> number in the array is a prime number . - *Done*
---
+++ run.cpp
@@ -0,0 +1,117 @@
+// Serial Number : 22
+//
+// Author : Luke Olson
+// Due Date : 04/06/20
+// Programming Assignment Number 4
+//
+// Spring 2020 - CS 3358 - 252
+//
+// Instructor: Husain Gholoom.
+//
+// Takes in size of array and completes multiple recursive functions
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <cmath>
+
+using namespace std;
+
+int size;
+
+void createArr(int, int x[]);
+void displayArr(int, int x[]);
+void reverseArr(int, int x[]);
+void displaySquares(int, int x[]);
+void digitSum(int, int x[]);
+void isPrime(int, int x[]);
+
+int main(){
+    cout << "*********************************************************\n"
+            "***	 Welcome	to	My	Recursion APP ***\n"
+            "*********************************************************\n\n"
+            "Enter The array size. ( Must be >= 5 ) -> ";
+    try{
+        cin >> size;
+        if(size<5){
+            cout << "\nInvalid arrays size. Size must >= 5." << endl;
+            return 0;
+        }
+        }catch(int e){
+            cout << "in catch" << endl;
+        }
+
+    int firstArr[0];
+    int reversedArr[0];
+
+    createArr(size, firstArr);
+    displayArr(size, firstArr);
+    reverseArr(size, reversedArr);
+    displaySquares(size, firstArr);
+    digitSum(size, firstArr);
+    isPrime(size, firstArr);
+
+    cout << "\nLuke Olson - Student Learning Algorithms " << endl
+         << "April 2020\n";
+}
+
+void createArr(int size, int arr[]){
+    int randNum;
+    srand (time(NULL));
+    for (int i = 0; i < size; i++) {
+
+        randNum = rand() % 375 + 10;
+        arr[i]=randNum;
+    }
+}
+
+void displayArr(int size, int arr[]){
+    cout << "\nThe generated array is  :";
+     for (int i = 0; i < size; i++) {
+        cout << "  " << arr[i]  << "  ";
+    }
+}
+
+void reverseArr(int size, int arr[]){
+    cout << "\n\nReversed array is  :";
+     for (int i = size-1; i > 0; i--) {
+        cout << "  " << arr[i]  << "  ";
+    }
+}
+
+void displaySquares(int size, int arr[]){
+    cout << "\n\nTable of square values   1  -   " <<  arr[0]+1 << endl;
+    cout << "\nTable of square values 1  -   " << arr[0];
+    cout << "\n\nN     N Squared" << endl;
+     for (int i = 1; i <= arr[0]+1; i++) {
+        cout << i << "     " << i*i << endl;
+    }
+}
+
+void digitSum(int size, int arr[]){
+    cout << "\nSum of its digits   is    ";
+    int sum = 0;
+     for (int i = 0; i < size; i++) {
+        sum+=1;
+    }
+    cout << sum << endl;
+}
+
+void isPrime(int size, int arr[]){
+    cout << "\nIs it prime :   \n\n";
+    for (int i = 1; i < size; i++) {
+        bool prime = true;
+        for(int j = 2; j <= arr[i] / 2; ++j){
+        if(arr[i] % j == 0){
+            prime = false;
+            break;
+            }
+        }
+        if(prime){
+            cout << "The number   " << arr[i] << "   is Prime" << endl;
+        }
+        else{
+            cout << "The number   " << arr[i] << "   is Not Prime" << endl;
+        }
+    }
+}
